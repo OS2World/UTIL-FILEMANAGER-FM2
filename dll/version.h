fce058cdc85353ae454e98168012c8bb0972e89c
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2002-10-16 23:38:00 +0000

    Add CheckVersion VERREALMINOR

--- dll/version.h
+++ dll/version.h
@@ -1,4 +1,21 @@
- #define VERMAJOR       3
- #define VERMINOR       02
+
+/***********************************************************************
+
+  $Id$
+
+  Version labels
+
+  Copyright (c) 1993-98 M. Kimes
+  Copyright (c) 2001, 2002 Steven H.Levine
+
+  Revisions	11 Jun 02 SHL - Add CheckVersion VERREALMINOR
+
+***********************************************************************/
+
+#define VERMAJOR       3
+#define VERMINOR       02
+#define VERREALMINOR   ""		// SHL
 
 #define APPNAME         "FM2"
+
+int CheckVersion (int vermajor,int verminor);
