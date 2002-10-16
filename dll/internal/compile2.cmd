8cff74287183ed704025545ca348953a34b91fe3
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2002-10-16 23:11:41 +0000

    Update for VAC 3.08

--- dll/internal/compile2.cmd
+++ dll/internal/compile2.cmd
@@ -1,1 +1,5 @@
-icc /G3 /O+ /W3 /Kb /Gs+ /Ss /Sm /B"/RUNFROMVDM" /B"/STACK:65535" /B"/BASE:0x10000" /B"/EXEPACK:2" /B"/ALIGN:4" %1
+::icc /G3 /O+ /W3 /Kb /Gs+ /Ss /Sm /B"/RUNFROMVDM" /B"/STACK:65535" /B"/BASE:0x10000" /B"/EXEPACK:2" /B"/ALIGN:4" %1
+set CFLAGS = /G5 /Gm+ /Gs- /Gt- /Mp /O+ /Q+ /Sm /Ss /W3 /C
+set LFLAGS = /EXEPACK:2 /MAP /PMTYPE:VIO
+icc %CFLAGS% /B"%LFLAGS%" %1
+
