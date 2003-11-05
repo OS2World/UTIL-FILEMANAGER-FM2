3d8273abaa7e1eae6629aceca5fe95f2a753c5b2
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2003-11-05 01:12:05 +0000

    Correct typo

--- reskit/makekit.cmd
+++ reskit/makekit.cmd
@@ -1,5 +1,5 @@
 zip -uj9 fm2rskt compile.cmd
-cd..\dll
+cd ..\dll
 zip -uj9 ..\reskit\fm2rskt fm3dll.str fm3str.h fm3res.rc fm3res.dlg fm3dlg.h fm3dll2.h internal\mkstr.exe
 zip -u9 ..\reskit\fm2rskt icons\*
 cd ..\reskit
