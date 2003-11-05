09f2923a0d5d818c99ffd1c460c2ebd0c12a5351
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2003-11-05 01:15:28 +0000

    v3.02c: misc icon cleanup

--- dll/version.h
+++ dll/version.h
@@ -9,12 +9,14 @@
   Copyright (c) 2001, 2002 Steven H.Levine
 
   Revisions	11 Jun 02 SHL - Add CheckVersion VERREALMINOR
+		11 Jun 03 SHL - v3.02b: add JFS and FAT32 support
+		04 Nov 03 SHL - v3.02c: misc icon cleanup
 
 ***********************************************************************/
 
 #define VERMAJOR       3
 #define VERMINOR       02
-#define VERREALMINOR   ""		// SHL
+#define VERREALMINOR   "c"		// SHL
 
 #define APPNAME         "FM2"
 
