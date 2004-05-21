ffed1acb6d8c4a37e2fbb0d41ef3f5dcf2f7cf67
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2004-05-21 15:16:36 +0000

    ShowMultimedia: try to convince fmplay to not play exes (Gregg Young)

--- dll/defview.c
+++ dll/defview.c
@@ -1,3 +1,17 @@
+
+/***********************************************************************
+
+  $Id$
+
+  Copyright (c) 1993-98 M. Kimes
+  Copyright (c) 2003 Steven H.Levine
+
+  Archive containers
+
+  Revisions	20 Nov 03 SHL - ShowMultimedia: try to convince fmplay to not play exes (Gregg Young)
+
+***********************************************************************/
+
 #define INCL_DOS
 #define INCL_WIN
 #define INCL_GPI
@@ -15,125 +29,125 @@
 #pragma alloc_text(DEFVIEW,DefaultView,ShowMultimedia,DefaultViewKeys)
 
 
-BOOL ShowMultimedia (CHAR *filename) {
+BOOL ShowMultimedia (CHAR *filename)
+{
 
-  static BOOL       unavailable = FALSE;
-  BOOL              ret = FALSE;
+  static BOOL       no_mmos2 = FALSE;
+  BOOL              played = FALSE;
   CHAR              loaderror[CCHMAXPATH];
-  HMODULE           MMIOModHandle = (HMODULE)0;
-  PMMIOIDENTIFYFILE MMIOIdentifyFile = (PMMIOIDENTIFYFILE)0;
-  FOURCC            fccStorageSystem = (FOURCC)0;
+  HMODULE           MMIOModHandle = NULLHANDLE;
+  PMMIOIDENTIFYFILE pMMIOIdentifyFile = NULL;
+  FOURCC            fccStorageSystem = 0;
   MMFORMATINFO      mmFormatInfo;
   APIRET            rc;
 
-  if(unavailable ||
-     !filename ||
-     !*filename)
-    return ret;
+  if (no_mmos2 ||
+      !filename ||
+      !*filename)
+    return played;
 
   /* load MMPM/2, if available. */
   *loaderror = 0;
   rc = DosLoadModule(loaderror,
-                     sizeof(loaderror),
-                     "MMIO",
-                     &MMIOModHandle);
-  if(rc) {
-    unavailable = TRUE;
-    return ret;
+		     sizeof(loaderror),
+		     "MMIO",
+		     &MMIOModHandle);
+  if (rc) {
+    no_mmos2 = TRUE;
+    return played;
   }
   else {
-    if(DosQueryProcAddr(MMIOModHandle,
-                        0,
-                        "mmioIdentifyFile",
-                        (PFN *)&MMIOIdentifyFile)) {
+    if (DosQueryProcAddr(MMIOModHandle,
+			0,
+			"mmioIdentifyFile",
+			(PFN *)&pMMIOIdentifyFile)) {
       DosFreeModule(MMIOModHandle);
-      unavailable = TRUE;
-      return ret;
+      no_mmos2 = TRUE;
+      return played;
     }
   }
 
   /* attempt to identify the file using MMPM/2 */
   memset(&mmFormatInfo,0,sizeof(MMFORMATINFO));
   mmFormatInfo.ulStructLen = sizeof(MMFORMATINFO);
-  rc = MMIOIdentifyFile(filename,
-                        0L,
-                        &mmFormatInfo,
-                        &fccStorageSystem,
-                        0L,
-                        MMIO_FORCE_IDENTIFY_FF);
+  rc = pMMIOIdentifyFile(filename,
+			 0L,
+			 &mmFormatInfo,
+			 &fccStorageSystem,
+			 0L,
+			 MMIO_FORCE_IDENTIFY_FF);
   /* free module handle */
   DosFreeModule(MMIOModHandle);
 
-  /* check returns from MMPM/2's identification process */
-  if(rc != MMIO_ERROR &&
-     mmFormatInfo.fccIOProc != FOURCC_DOS) {
-    if(mmFormatInfo.ulMediaType == MMIO_MEDIATYPE_IMAGE &&
-       (mmFormatInfo.ulFlags & MMIO_CANREADTRANSLATED) != 0) {
-      /* is an image */
+  /* if identified and not FOURCC_DOS */
+  if (!rc &&
+      mmFormatInfo.fccIOProc != FOURCC_DOS) {
+    if (mmFormatInfo.ulMediaType == MMIO_MEDIATYPE_IMAGE &&
+       (mmFormatInfo.ulFlags & MMIO_CANREADTRANSLATED)) {
+      /* is an image that can be translated */
       runemf2(SEPARATE | WINDOWED,
-              HWND_DESKTOP,
-              NULL,
-              NULL,
-              "%sIMAGE.EXE \"%s\"",
-              (fAddUtils) ? "UTILS\\" : NullStr,
-              filename);
-      ret = TRUE;
+	      HWND_DESKTOP,
+	      NULL,
+	      NULL,
+	      "%sIMAGE.EXE \"%s\"",
+	      (fAddUtils) ? "UTILS\\" : NullStr,
+	      filename);
+      played = TRUE;
     }
-    else {
-      if(mmFormatInfo.ulMediaType != MMIO_MEDIATYPE_IMAGE)
-        /* is a multimedia file (WAV, MID, AVI, etc.) */
-        runemf2(SEPARATE | WINDOWED,
-                HWND_DESKTOP,
-                NULL,
-                NULL,
-                "%sFM2PLAY.EXE \"%s\"",
-                (fAddUtils) ? "UTILS\\" : NullStr,
-                filename);
-      ret = TRUE;
+    else if (mmFormatInfo.ulMediaType != MMIO_MEDIATYPE_IMAGE) {
+      /* is a multimedia file (WAV, MID, AVI, etc.) */
+      runemf2(SEPARATE | WINDOWED,
+	      HWND_DESKTOP,
+	      NULL,
+	      NULL,
+	      "%sFM2PLAY.EXE \"%s\"",
+	      (fAddUtils) ? "UTILS\\" : NullStr,
+	      filename);
+      played = TRUE;
     }
   }
 
-  return ret;
+  return played;
 }
 
 
 VOID DefaultViewKeys (HWND hwnd,HWND hwndFrame,HWND hwndParent,
-                      SWP *swp,CHAR *filename) {
+		      SWP *swp,CHAR *filename) {
 
   if((shiftstate & (KC_CTRL | KC_SHIFT)) ==
      (KC_CTRL | KC_SHIFT))
     DefaultView(hwnd,
-                hwndFrame,
-                hwndParent,
-                swp,
-                4,
-                filename);
+		hwndFrame,
+		hwndParent,
+		swp,
+		4,
+		filename);
   else if(shiftstate & KC_CTRL)
     DefaultView(hwnd,
-                hwndFrame,
-                hwndParent,
-                swp,
-                2,
-                filename);
+		hwndFrame,
+		hwndParent,
+		swp,
+		2,
+		filename);
   else if(shiftstate & KC_SHIFT)
     DefaultView(hwnd,
-                hwndFrame,
-                hwndParent,
-                swp,
-                1,
-                filename);
+		hwndFrame,
+		hwndParent,
+		swp,
+		1,
+		filename);
   else
     DefaultView(hwnd,
-                hwndFrame,
-                hwndParent,
-                swp,
-                0,
-                filename);
+		hwndFrame,
+		hwndParent,
+		swp,
+		0,
+		filename);
 }
 
 
 VOID DefaultView (HWND hwnd,HWND hwndFrame,HWND hwndParent,SWP *swp,
-                  ULONG flags,CHAR *filename) {
+		  ULONG flags,CHAR *filename) {
 
   /*
    * bitmapped flags:
@@ -165,15 +179,15 @@ VOID DefaultView (HWND hwnd,HWND hwndFrame,HWND hwndParent,SWP *swp,
 
   if(flags & 2) {   /* open default WPS view of file */
     OpenObject(filename,
-               Default,
-               hwnd);
+	       Default,
+	       hwnd);
     return;
   }
 
   if(flags & 4) {   /* open WPS settings notebook for file */
     OpenObject(filename,
-               Settings,
-               hwnd);
+	       Settings,
+	       hwnd);
     return;
   }
 
@@ -184,30 +198,30 @@ VOID DefaultView (HWND hwnd,HWND hwndFrame,HWND hwndParent,SWP *swp,
 
     if(!IsFullName(filename)) {
       if(!DosQueryPathInfo(filename,
-                           FIL_QUERYFULLNAME,
-                           fullname,
-                           sizeof(fullname)))
-        filename = fullname;
+			   FIL_QUERYFULLNAME,
+			   fullname,
+			   sizeof(fullname)))
+	filename = fullname;
     }
     if(IsFullName(filename) &&
        !(driveflags[toupper(*filename) - 'A'] & DRIVE_INVALID)) {
       if(!IsRoot(filename)) {
-        dummy[0] = filename;
-        dummy[1] = NULL;
-        WinDlgBox(HWND_DESKTOP,
-                  HWND_DESKTOP,
-                  FileInfoProc,
-                  FM3ModHandle,
-                  FLE_FRAME,
-                  (PVOID)dummy);
+	dummy[0] = filename;
+	dummy[1] = NULL;
+	WinDlgBox(HWND_DESKTOP,
+		  HWND_DESKTOP,
+		  FileInfoProc,
+		  FM3ModHandle,
+		  FLE_FRAME,
+		  (PVOID)dummy);
       }
       else
-        WinDlgBox(HWND_DESKTOP,
-                  HWND_DESKTOP,
-                  DrvInfoProc,
-                  FM3ModHandle,
-                  INFO_FRAME,
-                  (PVOID)filename);
+	WinDlgBox(HWND_DESKTOP,
+		  HWND_DESKTOP,
+		  DrvInfoProc,
+		  FM3ModHandle,
+		  INFO_FRAME,
+		  (PVOID)filename);
     }
     return;
   }
@@ -222,98 +236,98 @@ VOID DefaultView (HWND hwnd,HWND hwndFrame,HWND hwndParent,SWP *swp,
       type = IDM_EDITBINARY;
     switch(type) {
       case IDM_EDITBINARY:
-        if(*bined) {
-          ExecOnList((HWND)0,
-                     bined,
-                     WINDOWED | SEPARATE,
-                     NULL,
-                     dummy,
-                     NULL);
-          break;
-        }
+	if(*bined) {
+	  ExecOnList((HWND)0,
+		     bined,
+		     WINDOWED | SEPARATE,
+		     NULL,
+		     dummy,
+		     NULL);
+	  break;
+	}
       /* else intentional fallthru */
       case IDM_EDITTEXT:
-        if(*editor)
-          ExecOnList((HWND)0,
-                     editor,
-                     WINDOWED | SEPARATE,
-                     NULL,
-                     dummy,
-                     NULL);
-        else {
-          type = (type == IDM_EDITTEXT) ? 8 :
-                  (type == IDM_EDITBINARY) ? 16 :
-                   0;
-          type |= 4;
-          StartMLEEditor(hwndParent,
-                         type,
-                         filename,
-                         hwndFrame);
-        }
-        break;
+	if(*editor)
+	  ExecOnList((HWND)0,
+		     editor,
+		     WINDOWED | SEPARATE,
+		     NULL,
+		     dummy,
+		     NULL);
+	else {
+	  type = (type == IDM_EDITTEXT) ? 8 :
+		  (type == IDM_EDITBINARY) ? 16 :
+		   0;
+	  type |= 4;
+	  StartMLEEditor(hwndParent,
+			 type,
+			 filename,
+			 hwndFrame);
+	}
+	break;
     }
     return;
   }
 
   if(ExecAssociation(hwnd,
-                     filename) == -1) {
+		     filename) == -1) {
     hwndArc = StartArcCnr((fExternalArcboxes || !swp ||
-                           strcmp(realappname,FM3Str)) ?
-                          HWND_DESKTOP :
-                          hwndParent,
-                          hwndFrame,
-                          filename,
-                          4,
-                          NULL);
+			   strcmp(realappname,FM3Str)) ?
+			  HWND_DESKTOP :
+			  hwndParent,
+			  hwndFrame,
+			  filename,
+			  4,
+			  NULL);
     if(!hwndArc) {
       if(!fCheckMM ||
-         !ShowMultimedia(filename)) {
-        if(!IsExecutable(filename) ||
-           !ExecFile(hwnd,filename)) {
-          p = strrchr(filename,'.');
-          if(!p)
-            p = ".";
-          if(stricmp(p,".INI") ||
-             !StartIniEditor(hwndParent,
-                             filename,
-                             4)) {
-            if(stricmp(p,".HLP") ||
-               !ViewHelp(filename)) {
+	 !ShowMultimedia(filename)) {
+	if(!IsExecutable(filename) ||
+	   !ExecFile(hwnd,filename)) {
+	  p = strrchr(filename,'.');
+	  if(!p)
+	    p = ".";
+	  if(stricmp(p,".INI") ||
+	     !StartIniEditor(hwndParent,
+			     filename,
+			     4)) {
+	    if(stricmp(p,".HLP") ||
+	       !ViewHelp(filename)) {
 ViewIt:
-              if(*viewer) {
-                dummy[0] = filename;
-                dummy[1] = NULL;
-                ExecOnList(hwnd,
-                           viewer,
-                           WINDOWED | SEPARATE |
-                           ((fViewChild) ? CHILD : 0),
-                           NULL,
-                           dummy,
-                           NULL);
-              }
-              else
-                StartMLEEditor(hwndParent,
-                               5,
-                               filename,
-                               hwndFrame);
-            }
-          }
-        }
+	      if(*viewer) {
+		dummy[0] = filename;
+		dummy[1] = NULL;
+		ExecOnList(hwnd,
+			   viewer,
+			   WINDOWED | SEPARATE |
+			   ((fViewChild) ? CHILD : 0),
+			   NULL,
+			   dummy,
+			   NULL);
+	      }
+	      else
+		StartMLEEditor(hwndParent,
+			       5,
+			       filename,
+			       hwndFrame);
+	    }
+	  }
+	}
       }
     }
     else {
       if((swp &&
-          !fExternalArcboxes &&
-          !strcmp(realappname,FM3Str)) &&
-         !ParentIsDesktop(hwnd,hwndParent))
-        WinSetWindowPos(hwndArc,
-                        HWND_TOP,
-                        swp->x,
-                        swp->y,
-                        swp->cx,
-                        swp->cy,
-                        SWP_MOVE   | SWP_SIZE     | SWP_SHOW |
-                        SWP_ZORDER | SWP_ACTIVATE);
+	  !fExternalArcboxes &&
+	  !strcmp(realappname,FM3Str)) &&
+	 !ParentIsDesktop(hwnd,hwndParent))
+	WinSetWindowPos(hwndArc,
+			HWND_TOP,
+			swp->x,
+			swp->y,
+			swp->cx,
+			swp->cy,
+			SWP_MOVE   | SWP_SIZE     | SWP_SHOW |
+			SWP_ZORDER | SWP_ACTIVATE);
     }
   }
 }
