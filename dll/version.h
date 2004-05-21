2f7f0426803be2bd0be3134482f0b3d96896ba71
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2004-05-21 15:54:29 +0000

    v3.02e: seeall, viewer traps v3.02f: dirsize applet large drivesth `CVS:' are removed automatically v3.02f: filldir: avoid spurious error reports v3.02g: valid::CheckDrive: debug removable detect

--- dll/version.h
+++ dll/version.h
@@ -6,17 +6,22 @@
   Version labels
 
   Copyright (c) 1993-98 M. Kimes
-  Copyright (c) 2001, 2002 Steven H.Levine
+  Copyright (c) 2001, 2004 Steven H.Levine
 
   Revisions	11 Jun 02 SHL - Add CheckVersion VERREALMINOR
 		11 Jun 03 SHL - v3.02b: add JFS and FAT32 support
 		04 Nov 03 SHL - v3.02c: misc icon cleanup
+		20 Nov 03 SHL - v3.02d: defview fixes
+		27 Nov 03 SHL - v3.02e: seeall, viewer traps
+		06 Jan 04 SHL - v3.02f: dirsize applet large drives
+		10 Jan 04 SHL - v3.02f: filldir: avoid spurious error reports
+		11 Mar 04 SHL - v3.02g: valid::CheckDrive: debug removable detect
 
 ***********************************************************************/
 
 #define VERMAJOR       3
 #define VERMINOR       02
-#define VERREALMINOR   "c"		// SHL
+#define VERREALMINOR   "g"		// SHL
 
 #define APPNAME         "FM2"
 
