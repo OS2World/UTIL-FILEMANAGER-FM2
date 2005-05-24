fdd3b9b93cd1d2abd20eb63dfd45c904339983b6
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2005-05-24 23:29:14 +0000

    Avoid datamin delays

--- dll/version.h
+++ dll/version.h
@@ -8,23 +8,24 @@
   Copyright (c) 1993-98 M. Kimes
   Copyright (c) 2001, 2005 Steven H.Levine
 
-  Revisions	11 Jun 02 SHL Add CheckVersion VERREALMINOR
-		11 Jun 03 SHL v3.02b: add JFS and FAT32 support
-		04 Nov 03 SHL v3.02c: misc icon cleanup
-		20 Nov 03 SHL v3.02d: defview fixes
-		27 Nov 03 SHL v3.02e: seeall, viewer traps
-		06 Jan 04 SHL v3.02f: dirsize applet large drives
-		10 Jan 04 SHL v3.02f: filldir: avoid spurious error reports
-		11 Mar 04 SHL v3.02g: valid::CheckDrive: debug removable detect
-		26 Jul 04 SHL v3.02h: valid::CheckDrive: debug invisible FAT32
-		31 Jul 04 SHL v3.02h: avv::ArcReviewDlgProc: correct filename flags updates
-		10 Jan 05 SHL v3.02h: Allow DND_TARGET to hold CCHMAXPATH
+  11 Jun 02 SHL Add CheckVersion VERREALMINOR
+  11 Jun 03 SHL v3.02b: add JFS and FAT32 support
+  04 Nov 03 SHL v3.02c: misc icon cleanup
+  20 Nov 03 SHL v3.02d: defview fixes
+  27 Nov 03 SHL v3.02e: seeall, viewer traps
+  06 Jan 04 SHL v3.02f: dirsize applet large drives
+  10 Jan 04 SHL v3.02f: filldir: avoid spurious error reports
+  11 Mar 04 SHL v3.02g: valid::CheckDrive: debug removable detect
+  26 Jul 04 SHL v3.02h: valid::CheckDrive: debug invisible FAT32
+  31 Jul 04 SHL v3.02h: avv::ArcReviewDlgProc: correct filename flags updates
+  10 Jan 05 SHL v3.02h: Allow DND_TARGET to hold CCHMAXPATH
+  23 May 05 SHL v3.02i: Avoid datamin delays
 
 ***********************************************************************/
 
 #define VERMAJOR       3
 #define VERMINOR       02
-#define VERREALMINOR   "h"		// SHL
+#define VERREALMINOR   "i"		// SHL
 
 #define APPNAME         "FM2"
 
