296077c30a1406563b7d64ac95a8b9114a0623cb
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2005-06-18 03:48:07 +0000

    Add missing items

--- reskit/makekit.cmd
+++ reskit/makekit.cmd
@@ -1,5 +1,10 @@
-zip -uj9 fm2rskt compile.cmd
+@echo off
+:: $Id$
+:: Make reskit.zip
+
+echo on
+zip -uj9 fm2rskt compile.cmd reskit.txt
 cd ..\dll
-zip -uj9 ..\reskit\fm2rskt fm3dll.str fm3str.h fm3res.rc fm3res.dlg fm3dlg.h fm3dll2.h internal\mkstr.exe
+zip -uj9 ..\reskit\fm2rskt fm3dll.str fm3str.h fm3res.rc fm3res.dlg fm3dlg.h fm3dll2.h fm3hlp.h internal\mkstr.exe
 zip -u9 ..\reskit\fm2rskt icons\*
 cd ..\reskit
