e76c58a8d2c1f3bd88b9207fd965d888a359c751
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2005-08-09 05:11:09 +0000

    common makefile prefix settings for all makefiles

--- makefile_pre.mk
+++ makefile_pre.mk
@@ -1,25 +1,24 @@
-# makefile_pre.mk - common settings for pre !include
+# makefile_pre.mk - common makefile prefix settings for all makefiles
+# $Id$
 
 LINK = ilink
 
-!IFNDEF DEBUG
+!ifndef DEBUG
 DEBUG = 0
-!ENDIF
+!endif
 
 CFLAGS = /G5 /Gm+ /Gs- /Gt- /IDLL /Mp /O- /Q+ /Sp4 /Ss /Ti+ /W3
 
-!IF $(DEBUG)
+!if $(DEBUG)
 LFLAGS = /DE /ALIGN:4 /EXEPACK:2 /NOI /MAP /PMTYPE:PM /NOE
-!ELSE
+!else
 LFLAGS = /ALIGN:4 /EXEPACK /MAP /NOI /PMTYPE:PM /NOE
-!ENDIF
+!endif
 
-# INCLUDE = $(INCLUDE);dll;
-
+# Includes can be in current director or dll subdirectory
 RCFLAGS = -i dll
 
 .SUFFIXES:
-
 .SUFFIXES: .c .rc .ipf
 
 .rc.res:
