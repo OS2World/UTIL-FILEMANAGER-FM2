1f7c0ab3cc24bc68cd5612d96da155b9619b4dc5
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2005-08-09 05:10:45 +0000

    common makefile suffix settings for all makefiles

--- makefile_post.mk
+++ makefile_post.mk
@@ -1,14 +1,36 @@
-# makefile_post.mk - common settings for post !include
+# makefile_post.mk - common makefile suffix settings for all makefiles
+# $Id$
+
+!ifndef MAKERES
 
 $(BASE).exe: $(BASE).obj $(BASE).res $(BASE).def
-    @REM @<<$(BASE).@0
-    $(LFLAGS)
-    $(BASE).obj
-    dll\fm3dll.lib os2386.lib
-    $(BASE).def
+  @rem @<<$(BASE).lrf
+  $(LFLAGS)
+  $(BASE).obj
+  dll\fm3dll.lib
+  os2386.lib
+  $(BASE).def
 <<
-    type $(BASE).@0
-    $(LINK) @$(BASE).@0
-    $(RC) -x2 $(BASE).res $(BASE).exe
+  type $(BASE).lrf
+  $(LINK) @$(BASE).lrf
+  $(RC) -x2 $(BASE).res $(BASE).exe
+
+!else
+
+$(BASE).exe: $(BASE).res
+  @if not exist $(BASE).exe echo $(BASE).exe missing
+  lxlite $(BASE).exe /x+ /b-
+  lxlite $(BASE).exe /c:minstub
+  $(RC) -x2 $(BASE).res $(BASE).exe
+  lxlite $(BASE).exe /x- /b-
+
+!endif
+
+clean:
+  -del $(BASE).exe
+  -del $(BASE).lrf
+  -del $(BASE).map
+  -del $(BASE).obj
+  -del $(BASE).res
 
 # The end
