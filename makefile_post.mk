f5295b5e57807178092edd9e8d32eb97b8639b29
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2005-08-16 15:14:49 +0000

    Clean up

--- makefile_post.mk
+++ makefile_post.mk
@@ -1,28 +1,31 @@
 # makefile_post.mk - common makefile suffix settings for all makefiles
 # $Id$
 
+# 16 Aug 05 SHL Clean up
+
 !ifndef MAKERES
 
 $(BASE).exe: $(BASE).obj $(BASE).res $(BASE).def
-  @rem @<<$(BASE).lrf
+  @$(LINK) @<<$(BASE).lrf
   $(LFLAGS)
   $(BASE).obj
   dll\fm3dll.lib
   os2386.lib
   $(BASE).def
 <<
-  type $(BASE).lrf
-  $(LINK) @$(BASE).lrf
-  $(RC) -x2 $(BASE).res $(BASE).exe
+  @rem type $(BASE).lrf
+  $(RC) -x2 $(BASE).res $@
+  bldlevel $@
 
 !else
 
 $(BASE).exe: $(BASE).res
-  @if not exist $(BASE).exe echo $(BASE).exe missing
-  lxlite $(BASE).exe /x+ /b-
-  lxlite $(BASE).exe /c:minstub
-  $(RC) -x2 $(BASE).res $(BASE).exe
-  lxlite $(BASE).exe /x- /b-
+  @if not exist $@ echo $@ missing
+  lxlite $@ /x+ /b-
+  lxlite $@ /c:minstub
+  $(RC) -x2 $(BASE).res $@
+  lxlite $@ /x- /b-
+  bldlevel $@
 
 !endif
 
