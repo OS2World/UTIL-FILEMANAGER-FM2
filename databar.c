f15d2901b9d4b341a6ad5e8778ba9c88ef451b2d
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2005-12-08 07:35:01 +0000

    Avoid warnings

--- databar.c
+++ databar.c
@@ -1,15 +1,33 @@
+
+/***********************************************************************
+
+  $Id$
+
+  databar applet
+
+  Copyright (c) 1993-98 M. Kimes
+  Copyright (c) 2005 Steven H. Levine
+
+  Oct 15 21:42:56 2002 967 ______ databar.c
+
+  15 Oct 02 SHL Baseline
+  07 Dec 05 SHL Avoid warnings
+
+***********************************************************************/
+
 #define INCL_DOS
 #define INCL_WIN
-
 #include <os2.h>
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "dll\fm3dll.h"
-#include "dll\fm3dlg.h"
 
+#include "dll\fm3dll.h"
+#include "dll\datamin.h"
+#include "dll\fm3dlg.h"
 
 #ifdef NEVER
 
@@ -26,7 +44,6 @@ VOID APIENTRY deinit (ULONG why) {
 
 #endif
 
-
 int main (int argc,char *argv[]) {
 
   HAB         hab;
@@ -34,9 +51,11 @@ int main (int argc,char *argv[]) {
   QMSG        qmsg;
 
   strcpy(appname,"DATABAR");
-#ifdef NEVER
+
+# ifdef NEVER
   DosExitList(EXLST_ADD,deinit);
-#endif
+# endif
+
   hab = WinInitialize(0);
   if(hab) {
     hmq = WinCreateMsgQueue(hab,384);
