17c3b6477a409b9795bb4c13516d035d2c3c9887
root <root@acaefd5c-9722-0410-a01d-a214f837194a>
2006-07-26 21:38:07 +0000

    Use Runtime_Error AddToList optimize

--- dll/makelist.c
+++ dll/makelist.c
@@ -6,27 +6,33 @@
   Make file lists
 
   Copyright (c) 1993-98 M. Kimes
-  Copyright (c) 2003 Steven H.Levine
+  Copyright (c) 2003, 2006 Steven H.Levine
 
-  Revisions	12 Feb 03 SHL - AddToFileList: standardize EA math
+  12 Feb 03 SHL AddToFileList: standardize EA math
+  22 Jul 06 SHL Use Runtime_Error
+  22 Jul 06 SHL AddToList optimize
 
 ***********************************************************************/
+
 #define INCL_DOS
 #define INCL_WIN
-
 #include <os2.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
 #include "fm3dll.h"
+#include "fm3str.h"
+
+static PSZ pszSrcFile = __FILE__;
 
 #pragma alloc_text(MAKELIST,AddToList,AddToFileList,BuildList,FreeListInfo,FreeList)
 #pragma alloc_text(MAKELIST,SortList,BuildArcList,RemoveFromList,CombineLists)
 
-
-VOID SortList (LISTINFO *li) {
-
+VOID SortList (LISTINFO *li)
+{
   /* bubble-sort entries by size, descending */
 
   INT   x;
@@ -58,8 +64,8 @@ VOID SortList (LISTINFO *li) {
 }
 
 
-VOID FreeListInfo (LISTINFO *li) {
-
+VOID FreeListInfo (LISTINFO *li)
+{
   if(li) {
     if(li->ulitemID)
       free(li->ulitemID);
@@ -72,8 +78,8 @@ VOID FreeListInfo (LISTINFO *li) {
 }
 
 
-VOID FreeList (CHAR **list) {
-
+VOID FreeList (CHAR **list)
+{
   register INT x;
 
   if(list) {
@@ -95,30 +101,36 @@ VOID FreeList (CHAR **list) {
 INT AddToFileList (CHAR *string,FILEFINDBUF4 *ffb4,FILELIST ***list,
                    INT *numfiles,INT *numalloced)
 {
-
-  FILELIST **test;
-
-  if(string && ffb4) {
-    if(((*numfiles) + 3) > *numalloced) {
-      if((test = realloc(*list,(((*numalloced) + 6) *
-          sizeof(FILELIST *)))) == NULL)
+  FILELIST *pfl;
+  if (string && ffb4) {
+    if (((*numfiles) + 3) > *numalloced) {
+      FILELIST **pflArray;
+      // Use plain realloc for speed
+      pflArray = realloc(*list,(*numalloced + 6) * sizeof(FILELIST *));
+      if (!pflArray) {
+        Runtime_Error(pszSrcFile,__LINE__,GetPString(IDS_OUTOFMEMORY));
         return 1;
+      }
       (*numalloced) += 6;
-      *list = test;
+      *list = pflArray;
     }
-    if(((*list)[*numfiles] = malloc(sizeof(FILELIST) +
-                                    strlen(string))) == NULL)
+    // Use plain malloc for speed
+    pfl = malloc(sizeof(FILELIST) + strlen(string));
+    if (!pfl) {
+      Runtime_Error(pszSrcFile,__LINE__,GetPString(IDS_OUTOFMEMORY));
       return 2;
-    (*list)[*numfiles]->attrFile = ffb4->attrFile;
-    (*list)[*numfiles]->date = ffb4->fdateLastWrite;
-    (*list)[*numfiles]->time = ffb4->ftimeLastWrite;
-    (*list)[*numfiles]->ladate = ffb4->fdateLastAccess;
-    (*list)[*numfiles]->latime = ffb4->ftimeLastAccess;
-    (*list)[*numfiles]->crdate = ffb4->fdateCreation;
-    (*list)[*numfiles]->crtime = ffb4->ftimeCreation;
-    (*list)[*numfiles]->cbFile = ffb4->cbFile;
-    (*list)[*numfiles]->easize = CBLIST_TO_EASIZE(ffb4->cbList);
-    strcpy((*list)[*numfiles]->fname,string);
+    }
+    pfl->attrFile = ffb4->attrFile;
+    pfl->date = ffb4->fdateLastWrite;
+    pfl->time = ffb4->ftimeLastWrite;
+    pfl->ladate = ffb4->fdateLastAccess;
+    pfl->latime = ffb4->ftimeLastAccess;
+    pfl->crdate = ffb4->fdateCreation;
+    pfl->crtime = ffb4->ftimeCreation;
+    pfl->cbFile = ffb4->cbFile;
+    pfl->easize = CBLIST_TO_EASIZE(ffb4->cbList);
+    strcpy(pfl->fname,string);
+    (*list)[*numfiles] = pfl;
     (*numfiles)++;
     (*list)[*numfiles] = NULL;
     (*list)[(*numfiles) + 1] = NULL;
@@ -130,19 +142,29 @@ INT AddToFileList (CHAR *string,FILEFINDBUF4 *ffb4,FILELIST ***list,
 }
 
 
-INT AddToList (CHAR *string,CHAR ***list,INT *numfiles,INT *numalloced) {
+INT AddToList (CHAR *string,CHAR ***list,INT *numfiles,INT *numalloced)
+{
+  CHAR **ppsz;
+  PSZ psz;
 
-  CHAR **test;
-
-  if(string) {
-    if(((*numfiles) + 3) > *numalloced) {
-      if((test = realloc(*list,(((*numalloced) + 6) * sizeof(CHAR *)))) == NULL)
+  if (string) {
+    if (((*numfiles) + 3) > *numalloced) {
+      // Use plain realloc for speed
+      ppsz = realloc(*list,(*numalloced + 6) * sizeof(CHAR *));
+      if (!ppsz) {
+        Runtime_Error(pszSrcFile, __LINE__, "realloc");
         return 1;
+      }
       (*numalloced) += 6;
-      *list = test;
+      *list = ppsz;
     }
-    if(((*list)[*numfiles] = malloc(strlen(string) + 1)) == NULL)
+    // Use plain malloc for speed
+    psz = malloc(strlen(string) + 1);
+    if (!psz) {
+      Runtime_Error(pszSrcFile,__LINE__,GetPString(IDS_OUTOFMEMORY));
       return 2;
+    }
+    (*list)[*numfiles] = psz;
     strcpy((*list)[*numfiles],string);
     (*numfiles)++;
     (*list)[*numfiles] = NULL;
@@ -155,8 +177,8 @@ INT AddToList (CHAR *string,CHAR ***list,INT *numfiles,INT *numalloced) {
 }
 
 
-CHAR ** BuildList (HWND hwndCnr) {
-
+CHAR ** BuildList (HWND hwndCnr)
+{
   PCNRITEM pci;
   CHAR   **list = NULL,**test;
   INT      numfiles = 0,numalloc = 0,error = 0,attribute = CRA_CURSORED;
@@ -170,22 +192,25 @@ CHAR ** BuildList (HWND hwndCnr) {
     }
   }
   while(pci && (INT)pci != -1 && !error) {
-    if(!(pci->rc.flRecordAttr & CRA_FILTERED))
+    if (!(pci->rc.flRecordAttr & CRA_FILTERED))
       error = AddToList(pci->szFileName,&list,&numfiles,&numalloc);
     pci = WinSendMsg(hwndCnr,CM_QUERYRECORDEMPHASIS,MPFROMP(pci),
                      MPFROMSHORT(attribute));
   }
-  if(numalloc > numfiles + 1) {
+  if (numalloc > numfiles + 1) {
+    // Use plain realloc for speed
     test = realloc(list,sizeof(CHAR *) * (numfiles + 1));
-    if(test)
+    if (!test)
+      Runtime_Error(pszSrcFile,__LINE__,GetPString(IDS_OUTOFMEMORY));
+    else
       list = test;
   }
   return list;
 }
 
 
-CHAR ** BuildArcList (HWND hwndCnr) {
-
+CHAR ** BuildArcList (HWND hwndCnr)
+{
   PARCITEM pai;
   CHAR   **list = NULL;
   INT      numfiles = 0,numalloc = 0,error = 0,attribute = CRA_CURSORED;
@@ -208,8 +233,8 @@ CHAR ** BuildArcList (HWND hwndCnr) {
 }
 
 
-CHAR ** RemoveFromList (CHAR **list,CHAR *item) {
-
+CHAR ** RemoveFromList (CHAR **list,CHAR *item)
+{
   register INT x,y;
 
   if(list && list[0] && item) {
@@ -234,8 +259,8 @@ CHAR ** RemoveFromList (CHAR **list,CHAR *item) {
 }
 
 
-CHAR ** CombineLists (CHAR **prime,CHAR **add) {
-
+CHAR ** CombineLists (CHAR **prime,CHAR **add)
+{
   register INT x;
   INT          numalloc,numfiles = 0;
 
