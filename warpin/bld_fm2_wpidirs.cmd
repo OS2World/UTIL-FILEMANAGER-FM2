8dd4b136adf36bb9d664660bc0c098dd58ebda21
jbs <jbs@acaefd5c-9722-0410-a01d-a214f837194a>
2007-04-18 22:42:21 +0000

    -  Changed to support improved WPI build process, including the    new format for BLD_FM2_WPIDIRS.TXT and using the new    BLD_FM2_WPIDIRS.IN file.

--- warpin/bld_fm2_wpidirs.cmd
+++ warpin/bld_fm2_wpidirs.cmd
@@ -4,6 +4,7 @@
  * FM/2 Warpin archive (WPI file).
  *
  * TODO:
+ *    -  Support multiple levels of destination directories
  *    -  Add logic to read script to determine
  *       -  Number of packages
  *       -  Package numbers
@@ -30,22 +31,21 @@ globals = 'WPI.'
 
 WPI. = ''
 WPI.retval = 0
-WPI.max_package_number = 999
+WPI.max_package_number_length = 3
 
 parse arg args
 call ProcessArgs strip(args)
 
 call ReadFile
 
-do p = 1 to WPI.pkg.0
-   dest_basedir   = 'PACKAGE.' || right(WPI.pkg.p.number, length(WPI.max_package_number), '0')
-   '@if not exist 'dest_basedir'\. md 'dest_basedir
-   if WPI.pkg.p.file.0 \= '' then
-      do f = 1 to WPI.pkg.p.file.0
-         dest_dir = dest_basedir || '\' || WPI.pkg.p.file.f.dest
-         '@if not exist 'dest_dir'\. md 'dest_dir
-         'copy ' || WPI.src_basedir || WPI.pkg.p.file.f.src || '\' || WPI.pkg.p.file.f.name || ' ' || dest_dir
-      end
+do f = 1 to WPI.fileline.0
+   parse var WPI.fileline.f . filename pkgnum src_subdir dest_subdir
+   if WPI.src_basedir \= '..\' then  /* Ignore src_subdir from file if not using build subtree */
+      src_subdir = '.'
+   dest_basedir   = MakePackageDir( pkgnum )
+   dest_dir = dest_basedir || '\' || strip(dest_subdir)
+   call SysMkDir dest_dir
+   'copy ' || WPI.src_basedir || src_subdir || '\' || filename || ' ' || dest_dir
 end
 
 if WPI.retval = 0 then
@@ -54,33 +54,19 @@ if WPI.retval = 0 then
 ErrorExit:
    parse arg errorcode
    select
-      when errorcode = 1 then
-         say 'Error: More than one PACKAGECOUNT line in 'WPI.infile
-      when errorcode = 2 then
-         say 'Error: More than PACKAGECOUNT PACKAGE lines in 'WPI.infile
       when errorcode = 3 then
          do
             say 'Error: Too many tokens on a line in 'WPI.infile
             say 'Line : 'txtline
          end
       when errorcode = 4 then
-         say 'Error: Keyword is not PACKAGECOUNT, PACKAGE or FILE in 'WPI.infile
+         say 'Error: Keyword is not FILE or NOFILESPACKAGE in 'WPI.infile
       when errorcode = 5 then
 novalue:
          do
             say 'Error: Unitialized value: "' || condition('D') || ' encountered on line: 'sigl
             say '   'sourceline(sigl)
             signal NormalExit
-         end
-      when errorcode = 6 then
-         do
-            say 'Error: Duplicate PACKAGE line in 'WPI.infile
-            say 'Line : 'txtline
-         end
-      when errorcode = 7 then
-         do
-            say 'Error: Invalid package number in 'WPI.infile
-            say 'Line : 'txtline
          end
       otherwise
          say 'Error: unknown error code: 'errorcode
@@ -94,7 +80,7 @@ NormalExit:
    exit WPI.retval
 
 ReadFile: procedure expose (globals)
-   package_number_list = ''
+   f = 0
    do while lines(WPI.infile) > 0
       txtline = strip(linein(WPI.infile))
       if txtline == '' then
@@ -107,54 +93,28 @@ ReadFile: procedure expose (globals)
             parse var txtline txtline ';' .     /* strip off comments */
             txtline = strip(txtline)
          end
-      word1 = translate(word(txtline, 1))
-      select
-         when word1 == 'PACKAGECOUNT:' then
-            if WPI.pkg.0 == '' then
-               WPI.pkg.0 = word(txtline, 2)
+      parse value translate(txtline) with word1 word2 word3 .
+      if (word1 == 'FILE:') then
+         do
+            if words(txtline) \= 5 then
+               call ErrorExit 3  /* Invalid number of parameters */
             else
-               call ErrorExit 1           /* More than one PACKAGECOUNT */
-         when word1 == 'PACKAGE:' then
-            do
-               parse var txtline . pkgnum pkgdesc
-               if wordpos(pkgnum, package_number_list) > 0 then
-                  call ErrorExit 6         /* Repeated package line */
-               package_number_list = package_number_list pkgnum
-               p = words(package_number_list)
-               if p > WPI.pkg.0 then
-                  call ErrorExit 2        /*  More packages than PACKAGECOUNT  */
-               WPI.pkg.p.number = pkgnum
-               WPI.pkg.p.desc = strip(pkgdesc)
-            end
-         when (word1 == 'FILE:') then
-            do
-               parse var txtline . filename pkgnum filesrc filedest rest
-               p = wordpos(pkgnum, package_number_list)
-               if p = 0 then
-                  call ErrorExit 7
-               else
-                  if strip(rest) \= '' then
-                     call ErrorExit 3  /* Too many parameters */
-                  else
-                     do
-                        i = WPI.pkg.p.file.0
-                        if i = '' then
-                           i = 1
-                        else
-                           i = i + 1
-                        WPI.pkg.p.file.0      = i
-                        WPI.pkg.p.file.i.name = filename
-                        if WPI.src_basedir \= '..\' then  /* Ignore src-dir if not using build subtree */
-                           WPI.pkg.p.file.i.src  = '.'
-                        else
-                           WPI.pkg.p.file.i.src  = filesrc
-                        WPI.pkg.p.file.i.dest = filedest
-                     end
-            end
-         otherwise
+               do
+                  f = f + 1
+                  WPI.fileline.f = strip(txtline)
+/*
+                  if word3 > WPI.max_package_number then
+                      WPI.max_package_number = word3
+*/
+               end
+         end
+      else
+         if word1 == 'NOFILESPACKAGE:' then
+            call MakePackageDir word2
+         else
             call ErrorExit 4        /* Invalid line/word1 */
-      end
    end
+   WPI.fileline.0 = f
    call stream WPI.infile, 'c', 'close'
 return
 
@@ -180,11 +140,20 @@ ProcessArgs: procedure expose (globals)
    if right(WPI.src_basedir, 1) \= '\' then
       WPI.src_basedir = WPI.src_basedir || '\'
    if args == '' then
+/*
       WPI.infile = 'BLD_FM2_WPIDIRS.TXT'
+*/
+      WPI.infile = 'BLD_FM2_WPIDIRS.IN'
    else
       if left(args, 1) == '"' then
          parse var args '"' WPI.infile '"' .
       else
          WPI.infile = word(args, 1)
 return
+
+MakePackageDir: procedure expose (globals)
+   parse arg pkgnum
+   full_dirname = 'PACKAGE.' || right(pkgnum, WPI.max_package_number_length, '0')
+   call SysMkDir full_dirname
+return full_dirname
 
