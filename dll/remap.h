3178937a3c445263be2f50eb508dbf221456d604
jbs <jbs@acaefd5c-9722-0410-a01d-a214f837194a>
2008-09-05 21:49:36 +0000

    Ticket 187: Draft 1: Functions only

---
+++ dll/remap.h
@@ -0,0 +1,21 @@
+
+/***********************************************************************
+
+$Id: $
+
+<<<description here>>>
+
+Copyright (c) 1993-98 M. Kimes
+Copyright (c) 2008 Steven H. Levine
+
+Change log
+
+***********************************************************************/
+
+#if !defined(REMAP_H)
+#define REMAP_H
+
+MRESULT EXPENTRY RemapDlgProc(HWND hwnd, ULONG msg, MPARAM mp1, MPARAM mp2);
+
+
+#endif // REMAP_H
