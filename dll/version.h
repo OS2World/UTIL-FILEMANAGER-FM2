b59980d284267578da26497a095fb1a95324998b
gyoung <gyoung@acaefd5c-9722-0410-a01d-a214f837194a>
2008-11-16 20:48:57 +0000

    Update .def files etc for 3.14 release

--- dll/version.h
+++ dll/version.h
@@ -54,7 +54,7 @@
 ***********************************************************************/
 
 #define VERMAJOR       3
-#define VERMINOR       13
+#define VERMINOR       14
 // Ensure VERREALMINOR nul for non-beta releases
 // #define VERREALMINOR   ""
 #define VERREALMINOR   " "__DATE__
