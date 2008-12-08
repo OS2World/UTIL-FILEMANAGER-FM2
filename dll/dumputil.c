8f144978f981c963711b7f9187ed7f3e0acb771f
stevenhl <stevenhl@acaefd5c-9722-0410-a01d-a214f837194a>
2008-12-08 21:38:54 +0000

    Ticket 307: add process dump support - see dumputil.c

---
+++ dll/dumputil.c
@@ -0,0 +1,39 @@
+
+/***********************************************************************
+
+  $Id: $
+
+  Process dump facility interface
+
+  Copyright (c) 2008 Steven H. Levine
+
+  06 Dec 08 SHL Baseline (Ticket 307)
+
+***********************************************************************/
+
+#define INCL_DOSMISC			// DosDumpProcess
+#include <os2.h>
+
+#include "errutil.h"
+#include "dumputil.h"
+
+// 06 Dec 08 SHL fixme to be in OpenWatcom bsedos.h
+APIRET APIENTRY DosDumpProcess (ULONG Flag, ULONG Drive, PID Pid);
+
+static PSZ pszSrcFile = __FILE__;
+
+/**
+ * Generate process dump if dump facility enabled
+ * @note Need to import DosDumpProcess = DOSCALLS.113
+ */
+
+VOID DumpProcess(VOID)
+{
+  APIRET apiret = DosDumpProcess(DDP_PERFORMPROCDUMP, 0, 0);
+  // Use DbgMsg to report errors - Dos_Error probably unsafe here
+  if (apiret) {
+    DbgMsg(pszSrcFile, __LINE__, "DosDumpProcess DDP_PERFORMPROCDUMP reported %u", apiret);
+  }
+}
+
+#pragma alloc_text(DUMPPROCESS, DumpProcess)
---
+++ dll/dumputil.h
@@ -0,0 +1,23 @@
+
+/***********************************************************************
+
+  $Id: $
+
+  Process dump facility interface
+
+  Copyright (c) 2008 Steven H. Levine
+
+  06 Dec 08 SHL Baseline (Ticket 307)
+
+***********************************************************************/
+
+#if !defined(DUMPUTIL_H)
+#define DUMPUTIL_H
+
+#if !defined(OS2_INCLUDED)
+#include <os2.h>
+#endif
+
+VOID DumpProcess(VOID);
+
+#endif // DUMPUTIL_HPP
