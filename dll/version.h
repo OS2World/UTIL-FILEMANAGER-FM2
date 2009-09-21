03aa03f7362cc5f37e4838d12c7f06d224058700
stevenhl <stevenhl@acaefd5c-9722-0410-a01d-a214f837194a>
2009-09-21 16:24:13 +0000

    Commit 3.17 readme, history and version.h

--- dll/version.h
+++ dll/version.h
@@ -9,15 +9,6 @@
   Copyright (c) 2001, 2008 Steven H. Levine
 
   11 Jun 02 SHL Add CheckVersion VERREALMINOR
-  11 Jun 03 SHL v3.02b: add JFS and FAT32 support
-  04 Nov 03 SHL v3.02c: misc icon cleanup
-  20 Nov 03 SHL v3.02d: defview fixes
-  27 Nov 03 SHL v3.02e: seeall, viewer traps
-  06 Jan 04 SHL v3.02f: dirsize applet large drives
-  10 Jan 04 SHL v3.02f: filldir: avoid spurious error reports
-  11 Mar 04 SHL v3.02g: valid::CheckDrive: debug removable detect
-  26 Jul 04 SHL v3.02h: valid::CheckDrive: debug invisible FAT32
-  31 Jul 04 SHL v3.02h: avv::ArcReviewDlgProc: correct filename flags updates
   10 Jan 05 SHL v3.02h: Allow DND_TARGET to hold CCHMAXPATH
   23 May 05 SHL v3.02i: Avoid datamin delays
   25 May 05 SHL v3.02i: Rework large file/drive support
@@ -50,12 +41,13 @@
   06 Jul 08 GKY v3.11 Release
   21 Jul 08 GKY v3.12
   01 Sep 08 SHL v3.13
-  08 Sep 08 SHL v3.14pre (shl)
+  10 May 09 GKY v3.16
+  20 Sep 09 GKY v3.17
 
 ***********************************************************************/
 
 #define VERMAJOR       3
-#define VERMINOR       16
+#define VERMINOR       17
 // Ensure VERREALMINOR nul for non-beta releases
 // #define VERREALMINOR   ""
 #define VERREALMINOR " "__DATE__
