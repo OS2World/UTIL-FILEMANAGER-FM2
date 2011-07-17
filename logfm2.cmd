e868cfa5f5ec725afdbd2051e168e255f8bc5734
jbs <jbs@acaefd5c-9722-0410-a01d-a214f837194a>
2011-07-17 16:39:48 +0000

    Corrected redirection syntax.

--- logfm2.cmd
+++ logfm2.cmd
@@ -1,11 +1,18 @@
 /*
-	Run FM/2 with logging enabled
-*/
+ * $Id$
+ *
+ * Run FM/2 with logging enabled
+ *
+ * Change log:
+ *    11 Jul 17 JBS Corrected redirection syntax
+ *
+ */
+	
 parse arg logfile
 fulllogfilename = stream(logfile, 'c', 'query exists')
 if fulllogfilename = '' then
    fulllogfilename = logfile
 call lineout fulllogfilename, date() time() 'Logging started...'
 call directory '..'
-'start fm3.exe 2>>&1 1>>'fulllogfilename
+'start fm3.exe 1>>'fulllogfilename '2>&1'
 return
